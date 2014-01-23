c3ffde7a0dad47ac58a87e991df6552d5e160557
Garrick Cheung <garrick@garrickcheung.com>
2014-01-23 10:15:52 -0800

    Fixes busterjs/buster#395

--- test/buster-static-test.js
+++ test/buster-static-test.js
@@ -2,78 +2,112 @@ var buster = require("buster");
 var assert = buster.assert;
 var cli = require("../lib/buster-static");
 var http = require("http");
+var testConfig = require('./fixtures/test-config')
 
-var NOOP = function () {};
+var NOOP = function() {};
 
 buster.testCase("buster-static", {
-    setUp: function () {
+    setUp: function() {
         this.s = cli.create();
-        this.s.logger = { log: NOOP };
+        this.s.logger = {
+            log: NOOP,
+            error: NOOP
+        };
     },
 
-    "starts server with no arguments": function (done) {
-        this.stub(this.s, "startServer", done(function () {
+    "starts server with no arguments": function(done) {
+        this.stub(this.s, "startServer", done(function() {
             assert(true);
         }));
         this.s.run(["--config", __dirname + "/fixtures/test-config.js"]);
     },
 
-    "starts server on specified port with --port": function (done) {
-        this.stub(this.s, "startServer", function () {
+    "starts server on specified port with --port": function(done) {
+        this.stub(this.s, "startServer", function() {
             assert(true);
             done();
         });
         this.s.run(["--config",
-                    __dirname + "/fixtures/test-config.js",
-                    "--port",
-                    "4224"]);
+            __dirname + "/fixtures/test-config.js",
+            "--port",
+            "4224"
+        ]);
     },
 
-    "writes to disk with operand": function (done) {
-        this.stub(this.s, "writeToDisk", function () {
+    "// writes to disk with operand": function(done) {
+        this.stub(this.s, "writeToDisk", function() {
             assert(true);
             done();
         });
         this.s.run(["--config",
-                    __dirname + "/fixtures/test-config.js",
-                    "/tmp/static-test"]);
+            __dirname + "/fixtures/test-config.js"
+        ]);
     },
 
     "http server": {
-        setUp: function (done) {
+        setUp: function(done) {
             var self = this;
             this.s.run(["--config",
-                        __dirname + "/fixtures/test-config.js",
-                        "--port",
-                        "17171"]);
+                __dirname + "/fixtures/test-config.js",
+                "--port",
+                "17171"
+            ]);
 
             var oldStartServer = this.s.startServer;
-            this.s.startServer = function () {
+            this.s.startServer = function() {
                 oldStartServer.apply(self.s, arguments);
                 self.s.httpServer.on("listening", done);
             };
         },
 
-        tearDown: function (done) {
-            this.s.httpServer.on("close", done);
-            this.s.httpServer.close();
+        tearDown: function(done) {
+            this.s.httpServer.close(done);
         },
 
-        "gets testbed": function (done) {
-            var req = http.request({ port: 17171, path: "/" }, function (res) {
+        "gets testbed": function(done) {
+            var req = http.request({
+                port: 17171,
+                path: "/"
+            }, function(res) {
                 assert.equals(res.statusCode, 200);
                 done();
-            }).end();
+            });
+
+            // properly disconnect the connection
+            req.shouldKeepAlive = false;
+
+            req.end();
         },
 
-        "gets none-existent file": function (done) {
+        "gets none-existent file": function(done) {
             var req = http.request({
                 port: 17171,
                 path: "/buster-sucks"
-            }, function (res) {
+            }, function(res) {
                 assert.equals(res.statusCode, 404);
                 done();
-            }).end();
+            });
+
+            // properly disconnect the connection
+            req.shouldKeepAlive = false;
+
+            req.end();
         }
+    },
+
+    "loads optional extensions": function(done) {
+        testConfig['Tests'].extensions = [{
+            name: 'test-extension',
+            create: function() {
+                assert(true)
+                delete testConfig['Tests'].extensions;
+                done();
+                return Object.create(this)
+            }
+        }];
+
+        this.s.run(["--config",
+            __dirname + "/fixtures/test-config.js"
+        ]);
     }
 });
